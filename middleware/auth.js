ba950dc8eafba035d87ba64570b726a264e9d870
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-07-15 22:03:41 +0530

    Auth Update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { stringToU8a, hexToU8a } = require("@polkadot/util");
+const { signatureVerify } = require("@polkadot/util-crypto");
 
 module.exports = function (req, res, next) {
   // Get token from header
-  const token = req.header("x-auth-token"); // Check if not token
+  const token = req.header("x-auth-token");
 
   if (!token) {
     return res.status(401).json({
@@ -11,9 +13,27 @@ module.exports = function (req, res, next) {
     });
   } // Verify token
 
+  const address = req.body.address;
+
+  if (!address) {
+    return res.status(401).json({
+      msg: "No address, authorization denied",
+    });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.sign = decoded.sign;
+
+    const sign = hexToU8a(decoded.sign);
+    const message = stringToU8a(process.env.MESSAGE);
+    const { isValid } = signatureVerify(message, sign, address);
+
+    if (!isValid) {
+      return res.status(401).json({ msg: "Invalid Signature" });
+    }
+
+    req.address = address;
+
     next();
   } catch (err) {
     res.status(401).json({
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,40 +3,19 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 require("dotenv").config();
 const { check, validationResult } = require("express-validator");
-const { stringToU8a, hexToU8a } = require("@polkadot/util");
-const { signatureVerify } = require("@polkadot/util-crypto");
 const jwt = require("jsonwebtoken");
 
 // @route   GET api/auth
 // @desc    Validate Signature
 // @access  Public
-router.get(
-  "/",
-  [auth, check("address", "Wallet Address is required").not().isEmpty()],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    try {
-      const address = req.body.address;
-      const sign = hexToU8a(req.sign);
-      const message = stringToU8a(process.env.MESSAGE);
-
-      const { isValid } = signatureVerify(message, sign, address);
-
-      if (!isValid) {
-        return res.status(401).json({ msg: "Invalid Signature" });
-      }
-
-      res.json({ msg: "Valid Signature" });
-    } catch (error) {
-      console.error(error.message);
-      res.status(500).send("Server Error");
-    }
+router.get("/", auth, (req, res) => {
+  try {
+    res.json({ msg: "Logged In" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
   }
-);
+});
 
 // @route   POST api/auth
 // @desc    Authenticate user & get token
